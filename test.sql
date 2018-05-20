c4d2e742f50355dca172124fb05126d212d2b7f0
TRETES-PC\Candra Triyadi <mailbox.candra@icloud.com>
2018-05-21 00:33:53 +0700

    add postman file

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20 Mei 2018 pada 15.49
+-- Generation Time: 20 Mei 2018 pada 19.28
 -- Versi Server: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -23,42 +23,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `member`
+-- Struktur dari tabel `product`
 --
 
-CREATE TABLE `member` (
-  `Id` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Phone` varchar(30) NOT NULL,
-  `Address` text NOT NULL
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `member`
+-- Dumping data untuk tabel `product`
 --
 
-INSERT INTO `member` (`Id`, `Name`, `Phone`, `Address`) VALUES
-(5, 'candra', '085855533343', 'jalan jalan');
+INSERT INTO `product` (`id`, `name`, `price`) VALUES
+(2, 'tahu brontak', 550),
+(3, 'singkong', 750);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `member`
+-- Indexes for table `product`
 --
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`Id`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `member`
+-- AUTO_INCREMENT for table `product`
 --
-ALTER TABLE `member`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
